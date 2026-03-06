fd963ed2bbb09e97e017bf0eda1675579c9cd9c5
Samra <samramuqadasvu1@gmail.com>
2026-03-06 15:22:29 +0500

    day4

--- src/auth.ts
+++ src/auth.ts
@@ -1,39 +1,51 @@
-import NextAuth from "next-auth"
-import Credentials from "next-auth/providers/credentials"
-import User from "./models/user.model"
- 
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+
+import connectDb from "./lib/db";
+import User from "./models/user.model";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Credentials({
-        credentials:{
-            email:{label:"email",type:"email"},
-            password:{label:"password" ,type:"password"},
+      credentials: {
+        email: { label: "email", type: "email" },
+        password: { label: "password", type: "password" },
+      },
 
-        },
-        authorize(credentials,request){
-            try {
-                await connectDb()
-                const email=credentials.email
-                const password=credentials.password as string
-                const user=await User.findOne
-                if(!user){
-                    throw new Error("user does not exist")
-                }
-                const isMatch=await bcrypt.compare(password,user.password)
-                if(!isMatch){
-                    throw new Error("Incorrect password")
-                }
-                return {
-                    id:user._id,
-                    email:user.email,
-                    name:user.name,
-                    role:user.role
-                }
-            } catch (error) {
+      async authorize(credentials) {
+        try {
+          await connectDb();
 
-                
-            }
+          const email = credentials.email;
+          const password = credentials.password;
+
+          const user = await User.findOne({ email });
+
+          if (!user) {
+            throw new Error("User does not exist");
+          }
+
+          const isMatch = await bcrypt.compare(password ,user.password);
+
+          if (!isMatch) {
+            throw new Error("Incorrect password");
+          }
+
+          return {
+            id: user._id.toString(),
+            email: user.email,
+            name: user.name,
+            role: user.role,
+          };
+        } catch (error) {
+          console.log(error);
+          return null;
         }
-    })
+      },
+    }),
   ],
-})
+  callbacks:{
+    
+  }
+});
