c9f1477a46718a798f40d7cff956d200c7b2ece8
Samra <samramuqadasvu1@gmail.com>
2026-03-10 11:35:01 +0500

    regform

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,13 +1,34 @@
-import { ArrowLeft } from 'lucide-react'
+'use client'
+import { ArrowLeft, Leaf } from 'lucide-react'
 import React from 'react'
-
-function RegisterForm() {
+import {motion} from "motion/react"
+ type propType={
+  previousStep:(s:number)=>void
+ }
+function RegisterForm({previousStep}:propType) {
   return (
     <div className='flex felx-col items-center justify-center min-h-screen px-6 py-10 bg-white relative'>
-      <div className=' absolute top-6 left-6 fex-items items-center  gap-2  text-green-700 hover-text-green-900 transition-colors cursor-pointer '>
+      <div className=' absolute top-6 left-6 fex-items items-center  gap-2  text-green-700 hover-text-green-900 transition-colors cursor-pointer ' onClick={()=>previousStep(1)}>
         <ArrowLeft className='w-5 h-5'/>
-        <span>Back</span>
+        <span className='font-medium'>Back</span>
       </div>
+      <motion.h1 
+      initial={{
+          y:-10,
+          opacity:0
+      }}
+      animate={{
+        y:0,
+        opacity:1
+      }}
+      transition={{
+        duration:0.6,
+      }}
+      
+      className='text-4xl font-extrabold text-green-700'>
+    Create account
+      </motion.h1>
+      <p className='text-gray-600 mb-8  flex items-center'>Join Snapcart toady<Leaf className='w-5 h-5 text-green-400'/></p>
     </div>
   )
 }
